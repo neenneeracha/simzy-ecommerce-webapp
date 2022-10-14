c1d734a5e33c9b8640ce101fee70703adb2edf1d
soniagtm <s.soniagtm@gmail.com>
2022-10-14 14:51:38 +0700

    add api for getting products and each product's details

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+// mount router at the specified path
+const router = require('./routes')
+app.use("/api/v1", router)
+
+// start a server and listen on specified port
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`)
+})
