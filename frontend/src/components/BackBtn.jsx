60253dbc5ee390b60a44ffd245c149603723469c
soniagtm <s.soniagtm@gmail.com>
2022-11-04 09:36:53 +0700

    Revert to 21d8345

--- frontend/src/components/BackBtn.jsx
+++ frontend/src/components/BackBtn.jsx
@@ -1,17 +1,9 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React from 'react'
 
 const BackBtn = () => {
   return (
-    <Button
-      className="d-block mx-auto w-75"
-      type="submit"
-      variant="outline-dark"
-      style={{ marginTop: "30px" }}
-    >
-      Back
-    </Button>
-  );
-};
+    <div>BackBtn</div>
+  )
+}
 
-export default BackBtn;
+export default BackBtn
